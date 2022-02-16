d8c7365586930322a27f3c7d34901402256a47ef
Ron <43244104+rons4@users.noreply.github.com>
2022-02-16 14:54:35 +0100

    Add files via upload

--- signl4-notification.sh
+++ signl4-notification.sh
@@ -6,34 +6,48 @@
 SIGNL4_URL=$2
 
 SIGNL4_MSG="{"
-for ((i = 3; i <= $#; i=$i+2 )); do
+for ((i = 3; i <= $#-2; i=$i+2 )); do
   SIGNL4_MSG+="\"${!i}\": "
   ((j=i+1))
   SIGNL4_MSG+="\"${!j}\""
 
   # External Id
-  if [[ "${!i}" == "S4ExternalId" ]]
+  if [[ "${!i}" == "ServiceStateId" ]]
   then
-	SIGNL4_MSG+=", \"X-S4-ExternalId\": \"${!j}\""
+	SIGNL4_MSG+=", \"X-S4-ExternalId\": \"Icinga2: ${!j}\""
   fi
 
   # Check for resolved state
-  if [[ "${!i}" == "Type" && "${!j}" == "Recovery" ]]
+  shopt -s nocasematch; if [[ "${!i}" == "Type" && "${!j}" == "Recovery" ]]
   then
 	SIGNL4_MSG+=", \"X-S4-Status\": \"resolved\""
   fi
-  
-  if (($i < $# - 2))
+
+  # Check for acknowledged state
+  shopt -s nocasematch; if [[ "${!i}" == "Type" && "${!j}" == "Acknowledgement" ]]
+  then
+	SIGNL4_MSG+=", \"X-S4-Status\": \"acknowledged\""
+  fi
+
+  # Check for problem state
+  shopt -s nocasematch; if [[ "${!i}" == "Type" && "${!j}" == "Problem" ]]
+  then
+	SIGNL4_MSG+=", \"X-S4-Status\": \"new\""
+  fi
+
+  # Source System
+  SIGNL4_MSG+=", \"X-S4-SourceSystem\": \"Icinga\""
+
+  if (($i < $#))
   then
 	SIGNL4_MSG+=", "
   fi
 done
-SIGNL4_MSG+=", \"X-S4-SourceSystem\": \"Icinga\""
-SIGNL4_MSG="$SIGNL4_MSG }"
+SIGNL4_MSG="$SIGNL4_MSG }\""
 
 #SIGNL4_MSG="{ \"$1\": \"$2\" }"
 
-#SIGNL4_MSG="{ \"Title\": \"Service $1 notification\", \"Host\": \"$2\", \"IP\": \"$3\", \"Service\": \"$4\", \"State\": \"$5\", \"Additional Info\":\"$6\", \"Icinga notification\": \"$7\", \"X-S4-Service\": \"Icinga\"  }"
-echo "$SIGNL4_MSG"
+#SIGNL4_MSG="{ \"Title\": \"Service $1 notification\", \"Host\": \"$2\", \"IP\": \"$3\", \"Service\": \"$4\", \"State\": \"$5\", \"Additional Info\":\"$6\", \"Nagios notification\": \"$7\", \"X-S4-Service\": \"Nagios\"  }"
+
 #Send message to SIGNL4
 curl -L -X POST -H "Content-type: application/json" --data "$SIGNL4_MSG" $SIGNL4_URL
