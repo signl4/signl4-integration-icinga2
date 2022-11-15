df05d78a55d8761039bd92eff2899d59e996a957
Ron <43244104+rons4@users.noreply.github.com>
2022-11-15 17:33:17 +0100

    Update signl4-notification.sh

--- signl4-notification.sh
+++ signl4-notification.sh
@@ -47,7 +47,7 @@ SIGNL4_MSG="$SIGNL4_MSG }\""
 
 #SIGNL4_MSG="{ \"$1\": \"$2\" }"
 
-#SIGNL4_MSG="{ \"Title\": \"Service $1 notification\", \"Host\": \"$2\", \"IP\": \"$3\", \"Service\": \"$4\", \"State\": \"$5\", \"Additional Info\":\"$6\", \"Icinga notification\": \"$7\", \"X-S4-Service\": \"Nagios\"  }"
+#SIGNL4_MSG="{ \"Title\": \"Service $1 notification\", \"Host\": \"$2\", \"IP\": \"$3\", \"Service\": \"$4\", \"State\": \"$5\", \"Additional Info\":\"$6\", \"Icinga notification\": \"$7\", \"X-S4-Service\": \"Icinga\"  }"
 
 #Send message to SIGNL4
 curl -L -X POST -H "Content-type: application/json" --data "$SIGNL4_MSG" $SIGNL4_URL
